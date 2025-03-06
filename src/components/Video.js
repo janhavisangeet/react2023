fb29e3eb73883fd5e184247cbd573bc4c973d9ec
janhavi <sangeetjanhavi@gmail.com>
2025-03-06 14:23:46 +0530

    change

--- src/components/Video.js
+++ src/components/Video.js
@@ -11,8 +11,8 @@ function Video({title,id,channel="Coder Dost",views,time,verified,children,editV
   return (
       <>
       <div className={`container ${theme}`}>
-      <button className='close' onClick={()=>dispatch({type:'DELETE',payload:id})}>X</button>  
-      <button className='edit' onClick={()=>editVideo(id)}>Edit</button>  
+      <button className='Close' onClick={()=>dispatch({type:'DELETE',payload:id})}>X</button>  
+      <button className='Edit' onClick={()=>editVideo(id)}>Edit</button>  
       <div className="pic">
       <img src={`https://picsum.photos/id/${id}/160/90`} alt="Katherine Johnson" />
       </div>
