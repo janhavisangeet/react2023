858a89b0f548f8b19ce94ad1f62bd89780e4f0e4
janhavi <sangeetjanhavi@gmail.com>
2025-03-06 13:27:33 +0530

    commit change

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,32 +1,32 @@
 import Video from "./Video";
-import PlayButton from "./PlayButton";
+import PlayButton from "./PlayButton.js";
 
-function VideoList({videos,deleteVideo,editVideo}){
+function VideoList({ videos, deleteVideo, editVideo }) {
 
-    return(
-        <>
-        {videos.map((video) => (
-            <Video
-              key={video.id}
-              title={video.title}
-              views={video.views}
-              time={video.time}
-              channel={video.channel}
-              verified={video.verified}
-              id={video.id}
-              deleteVideo={deleteVideo}
-              editVideo={editVideo}
-            >
-              <PlayButton
-                onPlay={() => console.log('Playing..',video.title)}
-                onPause={() => console.log('Paused..',video.title)}
-              >
-                {video.title}
-              </PlayButton>
-            </Video>
-          ))}
-          </>
-    )
+  return (
+    <>
+      {videos.map((video) => (
+        <Video
+          key={video.id}
+          title={video.title}
+          views={video.views}
+          time={video.time}
+          channel={video.channel}
+          verified={video.verified}
+          id={video.id}
+          deleteVideo={deleteVideo}
+          editVideo={editVideo}
+        >
+          <PlayButton
+            onPlay={() => console.log('Playing..', video.title)}
+            onPause={() => console.log('Paused..', video.title)}
+          >
+            {video.title}
+          </PlayButton>
+        </Video>
+      ))}
+    </>
+  )
 }
 
-export default VideoList
+export default VideoList;
